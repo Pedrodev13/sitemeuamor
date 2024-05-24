b647d7de939dbc2554a60a6d011869647880c09c
PedroDev <pedrodev0202@gmail.com>
2024-05-24 13:24:14 -0300

    corrigindo problemas

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -7,9 +7,9 @@ const Home = () => (
         <div className="my-10 bg-gradiente-to-bl from-rose to-transparent">
             <h2 className="font-Montserrat text-base">Nesse site, so terá um texto produzido pelo seu amor, hoje pela manhã!</h2>
         </div>
-        <svg className="w-10 h-10 animate-bounce" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+        {/* <svg className="w-10 h-10 animate-bounce" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
         <path fill-rule="evenodd" d="M12 2.25a.75.75 0 0 1 .75.75v16.19l2.47-2.47a.75.75 0 1 1 1.06 1.06l-3.75 3.75a.75.75 0 0 1-1.06 0l-3.75-3.75a.75.75 0 1 1 1.06-1.06l2.47 2.47V3a.75.75 0 0 1 .75-.75Z" clip-rule="evenodd" />
-        </svg>
+        </svg> */}
 
     </div>
 )
